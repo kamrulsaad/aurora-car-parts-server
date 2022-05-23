91a7f6d60dfc34212cf757080a24171ea26cd6eb
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2022-05-24 00:57:33 +0600

    created all products API

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import cors from 'cors'
+import 'dotenv/config'
+import jwt from 'jsonwebtoken'
+import { MongoClient, ServerApiVersion } from 'mongodb'
+const port = process.env.PORT || 5000
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@aurora-car-parts.jiuln.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+
+    await client.connect()
+
+    const productsCollection = client.db('aurora-car-parts').collection('products')
+
+    try{
+
+        app.get('/products', async(req, res) => {
+            const result = await productsCollection.find().toArray()
+            res.send(result)
+        })
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send('Hello form aurora car parts dealer shop')
+})
+
+app.listen(port, () => {
+    console.log(port);
+})
